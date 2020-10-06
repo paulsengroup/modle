d1d55210c2d5f51b2096e7ab6a1e875db829e12b
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2020-10-06 19:59:36 +0200

    Cleaned up ContactMatrix

--- src/contacts.cpp
+++ src/contacts.cpp
@@ -10,8 +10,8 @@
 
 namespace modle {
 
-ContactMatrix::ContactMatrix(uint32_t width, uint32_t length)
-    : _width(width), _length(length), _matrix(allocate_matrix()) {}
+ContactMatrix::ContactMatrix(uint32_t nrows, uint32_t ncols)
+    : _nrows(nrows), _ncols(ncols), _matrix(allocate_matrix()) {}
 
 void ContactMatrix::increment(uint32_t row, uint32_t col, uint32_t n) {
   uint32_t j = col;
@@ -20,7 +20,14 @@ void ContactMatrix::increment(uint32_t row, uint32_t col, uint32_t n) {
     j = row;
     i = j - col;
   }
-  if (i >= this->_matrix.size() || j >= this->_matrix[0].size()) return; // Temporary workaround to avoid segfaults
+  if (i >= this->_matrix.size() || j >= this->_matrix[0].size()) {
+    absl::FPrintF(
+        stderr,
+        "WARNING: ContactMatrix::increment: ignoring one increment for matrix[%lu][%lu] += %lu! "
+        "Reason: out of range (nrows=%lu; ncols=%lu;)\n",
+        i, j, n, this->_nrows, this->_ncols);
+    return;  // Temporary workaround to avoid segfaults
+  }
   assert(i < this->_matrix.size());
   assert(j < this->_matrix[0].size());
   this->_matrix[i][j] += n;
@@ -33,6 +40,13 @@ void ContactMatrix::set(uint32_t row, uint32_t col, uint32_t n) {
     j = row;
     i = j - col;
   }
+  if (i >= this->_matrix.size() || j >= this->_matrix[0].size()) {
+    absl::FPrintF(stderr,
+                  "WARNING: ContactMatrix::set: ignoring one update for matrix[%lu][%lu] = %lu! "
+                  "Reason: out of range (nrows=%lu; ncols=%lu;)\n",
+                  i, j, n, this->_nrows, this->_ncols);
+    return;  // Temporary workaround to avoid segfaults
+  }
   assert(i < this->_matrix.size());
   assert(j < this->_matrix[0].size());
   this->_matrix[i][j] = n;
@@ -44,6 +58,14 @@ void ContactMatrix::decrement(uint32_t row, uint32_t col, uint32_t n) {
   if (row > col) {
     j = row;
     i = j - col;
+  }
+  if (i >= this->_matrix.size() || j >= this->_matrix[0].size()) {
+    absl::FPrintF(
+        stderr,
+        "WARNING: ContactMatrix::increment: ignoring one decrement for matrix[%lu][%lu] -= %lu! "
+        "Reason: out of range (nrows=%lu; ncols=%lu;)\n",
+        i, j, n, this->_nrows, this->_ncols);
+    return;  // Temporary workaround to avoid segfaults
   }
   assert(i < this->_matrix.size());
   assert(j < this->_matrix[0].size());
@@ -62,15 +84,12 @@ void ContactMatrix::print() const {
 
 std::vector<std::vector<uint32_t>> ContactMatrix::allocate_matrix() const {
   std::vector<std::vector<uint32_t>> m;
-  m.reserve(this->_width);
+  m.reserve(this->_nrows);
 
-  for (uint32_t i = 0; i < this->_width; ++i) {
-    std::vector<uint32_t> v(this->_length + this->_width, 0);
+  for (uint32_t i = 0; i < this->_nrows; ++i) {
+    std::vector<uint32_t> v(this->_ncols, 0);
     m.emplace_back(std::move(v));
   }
-  assert(this->_matrix.size() == this->_width);
-  for (const auto &row : this->_matrix) assert(row.size() == this->_length + this->_width);
-
   return m;
 }
 
@@ -81,21 +100,16 @@ uint32_t ContactMatrix::get(uint32_t row, uint32_t col) const {
     j = row;
     i = j - col;
   }
-  assert(i < this->_matrix.size());
-  assert(j < this->_matrix[0].size());
+  if (i >= this->_matrix.size() || j >= this->_matrix[0].size()) return 0;
   return this->_matrix[i][j];
-}
-
-uint32_t ContactMatrix::compute_diagonal_length(uint64_t x, uint64_t y) {
-  return std::sqrt((x * x) + (y * y));
 }
 
 void ContactMatrix::print_symmetric_matrix() const {
   std::string buff;
   buff.reserve(64 * 1024 * 1024);
-  for (uint32_t y = 0; y < this->_length; ++y) {
+  for (uint32_t y = 0; y < this->_ncols; ++y) {
     //    absl::FPrintF(stderr, "%u\t", y);
-    for (uint32_t x = 0; x < this->_length; ++x) {
+    for (uint32_t x = 0; x < this->_ncols; ++x) {
       uint32_t j = x;
       uint32_t i = j - y;
       if (y > x) {
@@ -103,7 +117,7 @@ void ContactMatrix::print_symmetric_matrix() const {
         i = j - x;
       }
 
-      if (i >= this->_width) {
+      if (i >= this->_nrows) {
         buff += "0\t";
       } else {
         buff += std::to_string(this->_matrix[i][j]) + "\t";
@@ -122,10 +136,10 @@ void ContactMatrix::print_symmetric_matrix() const {
 
 std::vector<std::vector<uint32_t>> ContactMatrix::generate_symmetric_matrix() const {
   std::vector<std::vector<uint32_t>> m;
-  m.reserve(this->_length);
-  for (uint32_t y = 0; y < this->_length; ++y) {
-    std::vector<uint32_t> row(this->_length, 0);
-    for (uint32_t x = 0; x < this->_length; ++x) {
+  m.reserve(this->_ncols);
+  for (uint32_t y = 0; y < this->_ncols; ++y) {
+    std::vector<uint32_t> row(this->_ncols, 0);
+    for (uint32_t x = 0; x < this->_ncols; ++x) {
       uint32_t j = x;
       uint32_t i = j - y;
       if (y > x) {
@@ -133,7 +147,7 @@ std::vector<std::vector<uint32_t>> ContactMatrix::generate_symmetric_matrix() co
         i = j - x;
       }
 
-      if (i < this->_width) {
+      if (i < this->_nrows) {
         row[x] = this->_matrix[i][j];
       }
     }
@@ -142,10 +156,14 @@ std::vector<std::vector<uint32_t>> ContactMatrix::generate_symmetric_matrix() co
   return m;
 }
 
-uint32_t ContactMatrix::n_rows() const { return this->_matrix.size(); }
-uint32_t ContactMatrix::n_cols() const { return this->_matrix[0].size(); }
-uint32_t ContactMatrix::apparent_n_rows() const { return this->_length; }
-uint32_t ContactMatrix::apparent_n_cols() const { return this->_length; }
+uint32_t ContactMatrix::n_rows() const {
+  assert(this->_nrows == this->_matrix.size());
+  return this->_matrix.size();
+}
+uint32_t ContactMatrix::n_cols() const {
+  assert(this->_ncols == this->_matrix.at(0).size());
+  return this->_matrix[0].size();
+}
 
 void ContactMatrix::write_to_tsv(const std::string &path_to_file) const {
   auto gzf = gzopen(path_to_file.c_str(), "w");
@@ -173,31 +191,23 @@ void ContactMatrix::write_full_matrix_to_tsv(const std::string &path_to_file) co
     throw std::runtime_error(
         absl::StrFormat("Unable to open file '%s' for writing.", path_to_file));
   }
+  std::vector<uint32_t> row(this->_ncols, 0);
   std::string buff;
-  buff.reserve(1024 * 1024);
-  for (uint32_t y = 0; y < this->_length; ++y) {
+  for (uint32_t y = 0; y < this->_ncols; ++y) {
     //    absl::FPrintF(stderr, "%u\t", y);
-    for (uint32_t x = 0; x < this->_length; ++x) {
+    for (uint32_t x = 0; x < this->_ncols; ++x) {
       uint32_t j = x;
       uint32_t i = j - y;
       if (y > x) {
         j = y;
         i = j - x;
       }
-
-      if (i >= this->_width) {
-        buff += "0\t";
-      } else {
-        buff += absl::StrFormat("%lu\t", this->_matrix[i][j]);
-      }
+      if (i < this->_nrows)
+        row[j] = this->_matrix[i][j];
+      else
+        row[j] = 0;
     }
-    buff += "\n";
-    if (buff.size() >= 1024 * 1023) {
-      gzwrite(gzf, buff.c_str(), buff.size());
-      buff.clear();
-    }
-  }
-  if (!buff.empty()) {
+    buff = absl::StrFormat("%s\n", absl::StrJoin(row, "\t"));
     gzwrite(gzf, buff.c_str(), buff.size());
   }
   if (gzclose(gzf) != Z_OK) {
--- src/include/modle/contacts.hpp
+++ src/include/modle/contacts.hpp
@@ -7,7 +7,7 @@ namespace modle {
 
 class ContactMatrix {
  public:
-  ContactMatrix(uint32_t width, uint32_t length);
+  ContactMatrix(uint32_t nrows, uint32_t ncols);
   [[nodiscard]] uint32_t get(uint32_t row, uint32_t col) const;
   void set(uint32_t row, uint32_t col, uint32_t n = 1);
   void increment(uint32_t row, uint32_t col, uint32_t n = 1);
@@ -17,19 +17,15 @@ class ContactMatrix {
   [[nodiscard]] std::vector<std::vector<uint32_t>> generate_symmetric_matrix() const;
   [[nodiscard]] uint32_t n_cols() const;
   [[nodiscard]] uint32_t n_rows() const;
-  [[nodiscard]] uint32_t apparent_n_cols() const;
-  [[nodiscard]] uint32_t apparent_n_rows() const;
   void write_full_matrix_to_tsv(const std::string& path_to_file) const;
   void write_to_tsv(const std::string& path_to_file) const;
-  // Write method to output table directly, without conversions to "original" coordinates
 
  private:
-  uint64_t _width;
-  uint64_t _length;
+  uint64_t _nrows;
+  uint64_t _ncols;
   std::vector<std::vector<uint32_t>> _matrix;
 
   [[nodiscard]] std::vector<std::vector<uint32_t>> allocate_matrix() const;
-  [[nodiscard]] static uint32_t compute_diagonal_length(uint64_t x, uint64_t y);
 };
 
 }  // namespace modle
