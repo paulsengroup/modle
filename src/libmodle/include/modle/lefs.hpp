paulsengroup/modle

commit 02a62a2e9096abd8fbd2a8dcb580f00975db26ed

Bugfixes. Updated Dockerfile

src/libmodle/lefs.cpp
@@ -120,8 +120,8 @@ void ExtrusionUnit::bind(Chromosome* chr, uint32_t pos, DNA::Direction direction
     }
     this->set_stalls(n_stalls);
     // NOLINTNEXTLINE
-    assert(this->_blocking_barrier >= &this->_bin->get_all_extr_barriers().front() &&
-           this->_blocking_barrier <= &this->_bin->get_all_extr_barriers().back());
+    // assert(this->_blocking_barrier >= &this->_bin->get_all_extr_barriers().front() &&
+    //       this->_blocking_barrier <= &this->_bin->get_all_extr_barriers().back());
   }
 }
 

Dockerfile.release
@@ -26,16 +26,14 @@ RUN dnf update -y \
     && mkdir -p /usr/local/bin                                                  \
     && ln -s /tmp/conan_venv/bin/conan /usr/local/bin/conan                     \
     && mkdir "$build_dir" && cd "$build_dir"       \
-    && env CC=/usr/bin/clang                       \
-           CXX=/usr/bin/clang++                    \
-           LD=/usr/bin/ld.lld                      \
+    && env CC=/usr/bin/gcc                         \
+           CXX=/usr/bin/g++                        \
+           LD=/usr/bin/ld                          \
        cmake -DCMAKE_BUILD_TYPE=Release            \
              -DENABLE_IPO=OFF                      \
              -DWARNINGS_AS_ERRORS=OFF              \
              -DENABLE_TESTING=OFF                  \
              -DCMAKE_INSTALL_PREFIX='/usr/local'   \
-             -DCMAKE_C_COMPILER=/usr/bin/clang     \
-             -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
              -DCMAKE_CXX_FLAGS="-march=${march}"   \
              -DCMAKE_C_FLAGS="-march=${march}"     \
              -G 'Unix Makefiles' ..                \

src/contact_matrix/contacts_impl.hpp
@@ -413,7 +413,6 @@ std::pair<uint32_t, uint32_t> ContactMatrix<I>::write_to_tsv(const std::string &
                                                              std::string_view header,
                                                              int bzip2_block_size) const {
   std::ofstream fp(path_to_file, std::ios_base::binary);
-  assert(header.empty() || header.back() == '\n');
   boost::iostreams::filtering_ostream out;
   std::string buff;
   uint64_t raw_size = 0;
@@ -425,7 +424,11 @@ std::pair<uint32_t, uint32_t> ContactMatrix<I>::write_to_tsv(const std::string &
     out.push(fp);
     std::vector<I> row(this->_ncols, 0);
     if (!header.empty()) {
-      out.write(header.data(), static_cast<long>(header.size()));
+      if (absl::EndsWith(header, "\n")) {
+        fmt::print(out, header);
+      } else {
+        fmt::print(out, "{}\n", header);
+      }
     }
     for (auto i = 0UL; i < this->_nrows; ++i) {
       for (auto j = 0UL; j < this->_ncols; ++j) {
@@ -434,8 +437,8 @@ std::pair<uint32_t, uint32_t> ContactMatrix<I>::write_to_tsv(const std::string &
       }
       // TODO: Figure out how to get the raw size when using fmt::print
       // fmt::print(out, "{}\n", absl::StrJoin(row, "\t"));
-      buff = absl::StrCat(absl::StrJoin(row, "\t"), "\n");
-      out.write(buff.data(), static_cast<long>(buff.size()));
+      buff = absl::StrJoin(row, "\t");
+      fmt::print(out, "{}\n", buff);
       raw_size += buff.size();
     }
     if (!out || !fp) {

src/libmodle/dna.cpp
@@ -379,6 +379,7 @@ void Chromosome::sort_barriers_by_pos() {
 
 void Chromosome::write_contacts_to_tsv(std::string_view output_dir, bool force_overwrite) const {
   auto t0 = absl::Now();
+  std::filesystem::create_directories(output_dir);
   std::string path_to_outfile = std::filesystem::weakly_canonical(
       fmt::format("{}/{}_modle_cmatrix.tsv.bz2", output_dir, this->name));
   fmt::print(stderr, "Writing contact matrix for '{}' to file '{}'...\n", this->name,
@@ -404,6 +405,8 @@ void Chromosome::write_contacts_to_tsv(std::string_view output_dir, bool force_o
 void Chromosome::write_barriers_to_tsv(std::string_view output_dir, bool force_overwrite) const {
   auto path_to_outfile = std::filesystem::weakly_canonical(
       fmt::format("{}/{}.extrusion_barriers.tsv.bz2", output_dir, this->name));
+  fmt::print(stderr, "Writing extr. barrier coordinates for '{}' to file '{}'\n", this->name,
+             path_to_outfile);
   if (!force_overwrite && std::filesystem::exists(path_to_outfile)) {
     fmt::print(stderr, "File '{}' already exists. Pass --force to overwrite... SKIPPING.\n",
                path_to_outfile);

src/libmodle/genome.cpp
@@ -45,7 +45,8 @@ Genome::Genome(const config& c)
       _lefs(generate_lefs(c.number_of_lefs)),
       _chromosomes(init_chromosomes_from_file(c.diagonal_width)),
       _sampling_interval(c.contact_sampling_interval),
-      _randomize_contact_sampling(c.randomize_contact_sampling_interval) {}
+      _randomize_contact_sampling(c.randomize_contact_sampling_interval),
+      _nthreads(std::min(std::thread::hardware_concurrency(), c.nthreads)) {}
 
 std::vector<uint32_t> Genome::get_chromosome_lengths() const {
   std::vector<uint32_t> lengths;
@@ -87,20 +88,18 @@ uint64_t Genome::get_n_of_busy_lefs() const {
   return this->get_n_lefs() - this->get_n_of_free_lefs();
 }
 
-void Genome::write_contacts_to_file(std::string_view output_dir, bool force_overwrite) const {
-  auto nthreads = std::min(std::thread::hardware_concurrency(), this->get_n_chromosomes());
-  boost::asio::thread_pool tpool(nthreads);
-  fmt::print(stderr,
-             "Writing contact matrices for {} chromosome(s) in folder '{} using {} threads'...\n",
-             this->get_n_chromosomes(), output_dir, nthreads);
+void Genome::write_contacts_to_file(std::string_view output_dir, bool force_overwrite) {
+  fmt::print(stderr, "Writing contact matrices for {} chromosome(s) in folder '{}'...\n",
+             this->get_n_chromosomes(), output_dir);
+  boost::asio::thread_pool tpool(this->_nthreads);
   auto t0 = absl::Now();
   std::filesystem::create_directories(output_dir);
   for (const auto& chr : this->_chromosomes) {
     boost::asio::post(tpool, [&]() { chr.write_contacts_to_tsv(output_dir, force_overwrite); });
   }
   tpool.join();
-  fmt::fprintf(stderr, "DONE! Saved %lu contact matrices in %s\n", this->get_n_chromosomes(),
-               absl::FormatDuration(absl::Now() - t0));
+  fmt::print(stderr, "DONE! Saved {} contact matrices in {}.\n", this->get_n_chromosomes(),
+             absl::FormatDuration(absl::Now() - t0));
 }
 
 void Genome::write_extrusion_barriers_to_file(std::string_view output_dir,
@@ -300,10 +299,8 @@ uint64_t Genome::remove_chromosomes_wo_extr_barriers() {
 std::pair<double, double> Genome::run_burnin(double prob_of_rebinding,
                                              uint32_t target_n_of_unload_events,
                                              uint64_t min_extr_rounds) {
-  auto nthreads = std::min(std::thread::hardware_concurrency(), this->get_n_chromosomes());
-  boost::asio::thread_pool tpool(nthreads);
   std::vector<double> burnin_rounds(this->get_n_chromosomes());
-
+  boost::asio::thread_pool tpool(std::min(this->get_n_chromosomes(), this->_nthreads));
   for (auto nchr = 0U; nchr < this->get_n_chromosomes(); ++nchr) {
     boost::asio::post(tpool, [&, nchr]() {
       const auto& chr = this->_chromosomes[nchr];
@@ -369,20 +366,24 @@ std::pair<double, double> Genome::run_burnin(double prob_of_rebinding,
   return std::make_pair(avg_burnin_rounds, burnin_rounds_stdev);
 }
 
-void Genome::simulate_extrusion(uint32_t iterations, double target_contact_density) {
+void Genome::simulate_extrusion(uint32_t iterations, double target_contact_density,
+                                std::string_view output_dir, bool write_contacts_to_file,
+                                bool force_overwrite) {
   // If the simulation is set to stop when a target contact density is reached, set the number of
   // iterations to a very large number (2^32)
   if (target_contact_density != 0.0) {
     iterations = UINT32_MAX;
   }
-  // Create a reasonably sized thread pool
-  auto nthreads = std::min(std::thread::hardware_concurrency(), this->get_n_chromosomes());
-  boost::asio::thread_pool tpool(nthreads);
+
+  // Instantiate the thread pool to be used for simulation and IO operations
+  boost::asio::thread_pool tpool(std::min(this->_nthreads, this->get_n_chromosomes()));
 
   // Initialize variables for simulation progress tracking
   std::atomic<uint64_t> ticks_done{0};
   std::atomic<uint64_t> extrusion_events{0};
-  bool simulation_completed{false};
+  std::atomic<uint64_t> chromosomes_completed{0};
+  std::atomic<uint64_t> chromosomes_written_to_disk{0};
+  std::atomic<bool> simulation_completed{false};
   std::mutex m;  // This mutex is supposed to protect simulation_complete. As of C++17 we also need
                  // to acquire a lock when modifying the condition variable simulation_completed_cv
                  // (otherwise the change might not be communicated to waiting threads)
@@ -391,6 +392,8 @@ void Genome::simulate_extrusion(uint32_t iterations, double target_contact_densi
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wimplicit-int-float-conversion"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wimplicit-conversion"
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
   std::thread progress_tracker([&]() {  // This thread is used to periodically print the simulation
                                         // progress to stderr
     // The total number of ticks tot_ticks is set depending on whether or not the simulation is
@@ -403,31 +406,46 @@ void Genome::simulate_extrusion(uint32_t iterations, double target_contact_densi
     //    number is given by the sum of the target number of contacts for each of the
     //    chromosomes simulated, where the target number of contacts is defined as:
     //       target contact density * (matrix columns * matrix rows)
-    const uint64_t tot_ticks =
+    const long double tot_ticks =
         target_contact_density != 0.0
-            ? std::accumulate(this->_chromosomes.begin(), this->_chromosomes.end(), 0U,
-                              [&](auto accumulator, const auto& chr) {
-                                return accumulator +
-                                       std::llround(target_contact_density * chr.contacts.n_cols() *
-                                                    chr.contacts.n_rows());
-                              })
+            ? std::accumulate(
+                  this->_chromosomes.begin(), this->_chromosomes.end(), 0.0L,
+                  [&](auto accumulator, const auto& chr) {
+                    return accumulator +
+                           (target_contact_density * chr.contacts.n_cols() * chr.contacts.n_rows());
+                  })
             : iterations * this->get_n_chromosomes();
 
+    const auto t0 = absl::Now();
     while (true) {
       {  // Wait for 5 seconds or until the simulation terminates
         std::unique_lock<std::mutex> lk(m);
         simulation_completed_cv.wait_for(lk, std::chrono::seconds(5));  // NOLINT
-        if (simulation_completed) {  // If the simulation has been completed, return
-                                     // immediately, so that the main thread can join this thread
+        if (simulation_completed) {  // Stop printing updates once there are no chromosomes left,
+                                     // and return immediately, so that the main thread can join
+                                     // this thread
           // Unlocking here is probably unnecessary, as there's nothing waiting on this mutex
           lk.unlock();
           return;
         }
       }
-      // Print progress
-      fmt::print(stderr, FMT_STRING("Approx. {:.2f}% done ({:.2f}M extr. events/s)\n"),
-                 100.0 * ticks_done / tot_ticks, extrusion_events / 5.0e6 /* 5s * 1M */);  // NOLINT
-      extrusion_events = 0;
+      if (extrusion_events > 0) {  // Print progress
+        const auto progress = ticks_done / tot_ticks;
+        const auto throughput = extrusion_events / 5.0e6; /* 5s * 1M */  // NOLINT
+        const auto chr_being_written_to_disk = chromosomes_completed - chromosomes_written_to_disk;
+        const auto delta_t = absl::ToDoubleSeconds(absl::Now() - t0);
+        fmt::print(
+            stderr,
+            FMT_STRING("### ~{:.2f}% ###   {:.2f}M extr/sec - Simulation completed for "
+                       "{}/{} chromosomes{} - ETA {}.\n"),
+            100.0 * progress, throughput, chromosomes_completed, this->get_n_chromosomes(),
+            chr_being_written_to_disk == 0
+                ? ""
+                : fmt::format(" ({} chr. are currently being written to disk)",
+                              chr_being_written_to_disk),
+            absl::FormatDuration(absl::Seconds(delta_t / std::max(1.0e-06L, progress) - delta_t)));
+        extrusion_events = 0;
+      }
     }
   });
 #pragma GCC diagnostic pop
@@ -447,12 +465,11 @@ void Genome::simulate_extrusion(uint32_t iterations, double target_contact_densi
       // Calculate the number of contacts after which the simulation for the current chromosome is
       // stopped. This is set to a very large number (2^64) when the simulation is set to run for a
       // fixed number of iterations
-      const uint64_t target_n_of_contacts =
+      const auto target_n_of_contacts =
           target_contact_density != 0.0
-              ? static_cast<uint64_t>(std::llround(
-                    target_contact_density *
-                    static_cast<double>(chr.contacts.n_rows() * chr.contacts.n_cols())))
-              : UINT64_MAX;
+              ? target_contact_density *
+                    static_cast<double>(chr.contacts.n_rows() * chr.contacts.n_cols())
+              : std::numeric_limits<double>::max();
 
       // Variables to track the simulation progress for the current chromosome
       uint64_t local_extr_events_counter = 0;
@@ -516,18 +533,37 @@ void Genome::simulate_extrusion(uint32_t iterations, double target_contact_densi
           }
         }
       }
+      if (++chromosomes_completed == this->get_n_chromosomes()) {
+        {  // Notify the thread that is tracking the simulation progress that we are done simulating
+          std::scoped_lock<std::mutex> lk(m);
+          simulation_completed = true;
+        }
+        simulation_completed_cv.notify_all();
+      }
       fmt::print(stderr, FMT_STRING("DONE simulating loop extrusion on '{}'! Simulation took {}\n"),
                  chr.name, absl::FormatDuration(absl::Now() - t0));
+      if (write_contacts_to_file) {
+        chr.write_contacts_to_tsv(output_dir, force_overwrite);
+        chr.write_barriers_to_tsv(output_dir, force_overwrite);
+        ++chromosomes_written_to_disk;
+      }
+      // Free up resources
+      // chr = Chromosome{"", 0, 0, 0};
     });
   }
   // This blocks until all the tasks posted to the thread_pool have been completed
   tpool.join();
-
-  {  // Notify the thread that is tracking simulation progress that we are done, then join it
-    std::scoped_lock<std::mutex> lk(m);
-    simulation_completed = true;
-  }
-  simulation_completed_cv.notify_all();
   progress_tracker.join();
+}
+
+void Genome::simulate_extrusion() { this->simulate_extrusion(1, 0, "", false, true); }
+void Genome::simulate_extrusion(uint32_t iterations, std::string_view output_dir,
+                                bool force_overwrite, bool write_contacts_to_file) {
+  this->simulate_extrusion(iterations, 0, output_dir, write_contacts_to_file, force_overwrite);
+}
+void Genome::simulate_extrusion(double target_contact_density, std::string_view output_dir,
+                                bool force_overwrite, bool write_contacts_to_file) {
+  this->simulate_extrusion(0, target_contact_density, output_dir, write_contacts_to_file,
+                           force_overwrite);
 }
 }  // namespace modle

src/libmodle/include/modle/config.hpp
@@ -8,6 +8,7 @@
 #include <cstdint>
 #include <filesystem>
 #include <string_view>
+#include <thread>
 
 namespace modle {
 using namespace std::literals::string_view_literals;
@@ -34,6 +35,7 @@ struct config {
   uint32_t contact_sampling_interval{1000};
   bool randomize_contact_sampling_interval{false};
   bool skip_output{false};
+  uint32_t nthreads{std::thread::hardware_concurrency()};
 
   int argc;
   char** argv;
@@ -58,8 +60,8 @@ struct config {
     std::vector<cli_tokens>{
      {"Bin size (bp)", fmt::format(FMT_STRING("{}"), this->bin_size)},
      {"Diagonal width (bp)", fmt::format(FMT_STRING("{}"), this->diagonal_width)},
-     {"# of iterations", fmt::format(FMT_STRING("{}"), this->target_contact_density != 0 ? "Disabled"sv : fmt::format("{}", this->simulation_iterations))},
-     {"Target contact density", fmt::format(FMT_STRING("{}"), this->target_contact_density != 0 ? fmt::format("{}", this->simulation_iterations) : "Disabled"sv)},
+     {"# of iterations", fmt::format(FMT_STRING("{}"), this->simulation_iterations != 0 ? "Disabled"sv : fmt::format("{}", this->simulation_iterations))},
+     {"Target contact density", fmt::format(FMT_STRING("{}"), this->target_contact_density != 0 ? fmt::format("{}", this->target_contact_density) : "Disabled"sv)},
      {"Seed", fmt::format(FMT_STRING("{}"), this->seed)},
      {"Randomize contact sampling interval", this->randomize_contact_sampling_interval ? "Yes" : "No"},
      {"Contact sampling interval", fmt::format(FMT_STRING("{}"), this->contact_sampling_interval)}}},

src/libmodle/include/modle/genome.hpp
@@ -43,7 +43,7 @@ class Genome {
   [[nodiscard]] std::vector<double> get_chromosome_lef_affinities() const;
   [[nodiscard]] uint64_t get_n_of_free_lefs() const;
   [[nodiscard]] uint64_t get_n_of_busy_lefs() const;
-  void write_contacts_to_file(std::string_view output_dir, bool force_overwrite) const;
+  void write_contacts_to_file(std::string_view output_dir, bool force_overwrite);
   void write_extrusion_barriers_to_file(std::string_view output_dir, bool force_overwrite) const;
 
   /** Randomly generate and bind \p n_barriers ExtrusionBarrier%s
@@ -95,7 +95,11 @@ class Genome {
    *
    * @param iterations Number of loop extrusion events to simulate
    */
-  void simulate_extrusion(uint32_t iterations = 1, double target_contact_density = 0.0);
+  void simulate_extrusion();
+  void simulate_extrusion(uint32_t iterations, std::string_view output_dir, bool force_overwrite,
+                          bool write_contacts_to_file = true);
+  void simulate_extrusion(double target_contact_density, std::string_view output_dir,
+                          bool force_overwrite, bool write_contacts_to_file = true);
 
  private:
   uint64_t _seed;
@@ -111,7 +115,11 @@ class Genome {
   std::vector<Chromosome> _chromosomes;
   uint32_t _sampling_interval;
   bool _randomize_contact_sampling;
-  // std::bernoulli_distribution _sample_contacts;
+  uint32_t _nthreads;
+
+  void simulate_extrusion(uint32_t iterations, double target_contact_density,
+                          std::string_view output_dir, bool write_contacts_to_file,
+                          bool force_overwrite);
 
   // Private initializers
   [[nodiscard]] std::vector<Chromosome> init_chromosomes_from_file(uint32_t diagonal_width) const;

src/modle/cli.hpp
@@ -52,6 +52,13 @@ class Cli {
             ->capture_default_str();
 
     gen->add_option(
+        "-t,--threads",
+        this->_config.nthreads,
+        "Max size of the thread pool to use to run the simulation. By default ModLE will attempt to use threads available.")
+        ->check(CLI::PositiveNumber)
+        ->capture_default_str();
+
+    gen->add_option(
             "-w,--diagonal-width",
             this->_config.diagonal_width,
             "Width of the matrix that will be used to store contacts, which visually corresponds to the width of the diagonal in a typical square contact matrix.")

commit 57667405215cf9e53d57c2d382976a21c006b5be

Fixed bug introduced by 02a62a2, that produced an undesired checkerboard pattern in the cmatrix

src/libmodle/include/modle/lefs.hpp
@@ -79,7 +79,7 @@ class Lef {
   // This will be used to add an offset of -2, -1, +1, +2 when binding to a bin. This is needed to
   // avoid the undesired zebra pattern described in #3
   // std::discrete_distribution<int8_t> _bin_idx_offset_generator{{1, 1, 0, 1, 1}};
-  std::uniform_int_distribution<int8_t> _bin_idx_offset_generator{-2, 2};
+  std::uniform_int_distribution<int64_t> _bin_idx_offset_generator{-2, 2};
   std::uniform_real_distribution<> _prob_of_rebind_generator{0.0, 1.0};
   uint64_t _binding_pos{UINT64_MAX};
   /// I am using a unique_ptr instead of storing the extr. units directly to avoid the cyclic

src/libmodle/lefs.cpp
@@ -221,14 +221,16 @@ void Lef::bind_at_pos(Chromosome* chr, uint32_t pos, std::mt19937& rand_eng,
   this->_chr = chr;
   this->_binding_pos = pos;
   this->_lifetime = this->_lifetime_generator(rand_eng);
-  const auto bin_idx_offset = this->_bin_idx_offset_generator(rand_eng);
+  const auto pos_offset =
+      this->_bin_idx_offset_generator(rand_eng) * static_cast<int64_t>(this->_chr->get_bin_size());
+
   // It would be nice to remove some static casts here, but the first static_cast to int64_t is
   // required to deal with the possibility that pos - offset overflows
   const auto pos1 = static_cast<uint32_t>(
-      std::clamp(bin_idx_offset < 0 ? static_cast<int64_t>(pos) + bin_idx_offset : pos, 0L,
+      std::clamp(pos_offset < 0 ? static_cast<int64_t>(pos) + pos_offset : pos, 0L,
                  static_cast<int64_t>(this->_chr->length())));
   const auto pos2 = static_cast<uint32_t>(
-      std::clamp(bin_idx_offset > 0 ? static_cast<int64_t>(pos) + bin_idx_offset : pos, 0L,
+      std::clamp(pos_offset > 0 ? static_cast<int64_t>(pos) + pos_offset : pos, 0L,
                  static_cast<int64_t>(this->_chr->length())));
   // We assume that the left unit always travels towards the 5', while the right unit goes to the 3'
   this->_left_unit->bind(this->_chr, pos1, DNA::Direction::rev, rand_eng);