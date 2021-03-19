598ee16467406747ea6e8a3153d524b151a89e5d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-03-19 12:40:31 +0100

    Update PCH

---
+++ scripts/count_headers.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+wd="$(dirname "$0")"
+
+if [ $# -gt 2 ]; then
+    echo >&2 "Usage:   $0 path_relative_to_repo_root/"
+    echo >&2 "Default: $0 src/"
+fi
+
+
+path='src/'
+if [ $# -eq 2 ]; then
+    path="$1"
+fi
+
+cat $(find "$wd/../$1" -type f -name "*.hpp" | tr '\n' ' ') | grep -oE '#include .[[:alnum:]_/\.\-]+.' | sort | uniq -c | sort -r
