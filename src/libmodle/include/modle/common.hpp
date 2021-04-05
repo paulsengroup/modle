c09bf09e9930a54a1997493b4766aa675059fafd
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-04-05 12:04:16 +0200

    Move PRNG stuff to modle/common.hpp

--- src/libmodle/include/modle/common.hpp
+++ src/libmodle/include/modle/common.hpp
@@ -3,7 +3,27 @@
 #include <cstddef>  // IWYU pragma: keep for size_t
 #include <cstdint>  // for uint32_t, uint_fast8_t
 
+#ifdef USE_XOSHIRO
+#include <Xoshiro-cpp/XoshiroCpp.hpp>  // for XoshiroCpp::Xoshiro256PlusPlus XoshiroCpp::SplitMix64
+#else
+#include <random>  // for mt19937_64, seed_seq
+#endif
+
 namespace modle {
+
+#ifdef USE_XOSHIRO
+[[nodiscard]] inline XoshiroCpp::Xoshiro256PlusPlus PRNG(uint64_t seed) {
+  auto seeder = XoshiroCpp::SplitMix64(seed);
+  return XoshiroCpp::Xoshiro256PlusPlus(seeder.generateSeedSequence<4>());
+}
+#else
+[[nodiscard]] inline std::mt19937_64 PRNG(uint64_t seed) {
+  auto seeder = std::seed_seq{seed};
+  return std::mt19937_64{seeder};
+}
+#endif
+
+using PRNG_t = decltype(std::function{PRNG})::result_type;
 
 using bp_t = std::size_t;
 using Contacts = uint32_t;
