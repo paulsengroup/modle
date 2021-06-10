912f0f5c27ed1f7d27afba86b69c6f22dd40643f
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-06-10 13:20:42 +0200

    Import cgranges as external dependency for modle_tools

--- cmake/FetchExternalDeps.cmake
+++ cmake/FetchExternalDeps.cmake
@@ -1,5 +1,11 @@
 include(FetchContent)
 FetchContent_Declare(
+  cgranges
+  URL ${CMAKE_CURRENT_SOURCE_DIR}/external/cgranges-10-06-2021.tar.xz
+  URL_HASH
+    SHA512=287006ec02a9e8b4e67f5bcf31b4c039123e33f4ff4e14405e44b053c24556b1117c02cd2a1c2c4c205f81a1989852ae2b7219ac8be0af92275f9dc9747303d2
+)
+FetchContent_Declare(
   libBigWig
   URL ${CMAKE_CURRENT_SOURCE_DIR}/external/libBigWig-0.4.6.tar.xz
   URL_HASH
@@ -12,8 +18,16 @@ FetchContent_Declare(
     SHA512=b8f7786f56733a284cf678142d0df232dba80d46e427d80bc360cc853c8617d80da3c49bf4654fea6635119fb4b86532a777ca66226262363537bd8625530354
 )
 
+FetchContent_GetProperties(cgranges)
 FetchContent_GetProperties(libBigWig)
 FetchContent_GetProperties(Xoshiro)
+
+if(MODLE_BUILD_UTILS)
+  if(NOT ${cgranges}_POPULATED)
+    FetchContent_Populate(cgranges)
+  endif()
+  add_subdirectory(${cgranges_SOURCE_DIR} ${cgranges_BINARY_DIR})
+endif()
 
 if(NOT ${libbigwig}_POPULATED)
   FetchContent_Populate(libBigWig)
