0b48a0a3ffa7e539bf115e0783d00aaab7bf000a
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-07-03 12:43:19 +0200

    Consolidate compiler/stdlib checks into one file and add check for <variant>
    [no ci]

--- cmake/TestStdlibFeatures.cmake
+++ cmake/TestStdlibFeatures.cmake
@@ -1,6 +1,41 @@
 include(CheckCXXSourceCompiles)
 
 set(CMAKE_REQUIRED_FLAGS, "-std=c++17")
+
+#
+# Detect stdlib on *NIX systems
+#
+check_cxx_source_compiles(
+  "#include <ciso646>
+
+     #if defined __GLIBCXX__
+        int a = 1;
+     #else
+        int a = x;
+     #endif
+
+     int main(int argc, char* argv[]) {
+        return 0;
+     }"
+  USING_STDLIBCXX)
+
+check_cxx_source_compiles(
+  "#include <ciso646>
+
+     #if defined _LIBCPP_VERSION
+        int a = 1;
+     #else
+        int a = x;
+     #endif
+
+     int main(int argc, char* argv[]) {
+        return 0;
+     }"
+  USING_LIBCXX)
+
+#
+# Detect <charconv> availability
+#
 check_cxx_source_compiles(
   "#include <charconv>
 
@@ -26,6 +61,21 @@ check_cxx_source_compiles(
    }"
   CHARCONV_FP_AVAILABLE)
 
+#
+# Detect <variant> availablilty
+#
+check_cxx_source_compiles(
+  "#include <variant>
+
+     int main(int argc, char* argv[]) {
+        std::variant<int, long> x{};
+        return 0;
+     }"
+  VARIANT_AVAILABLE)
+
+#
+# Detect whether <filesystem> requires explicit linking
+#
 check_cxx_source_compiles(
   "#include <ciso646>
 
