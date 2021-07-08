eaa2f677c973dae4f167c3010670146d37bb09f7
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-07-08 10:52:01 +0200

    Minor improvements to modle_tools CLI [no ci]

--- src/modle_tools/cli.hpp
+++ src/modle_tools/cli.hpp
@@ -45,6 +45,9 @@ class Cli {
   absl::btree_set<std::string> _filtering_criteria{"intersection", "pairwise-intersection"};
 
   inline void make_eval_subcommand() {
+    auto remove_trailing_zero_from_floats = [](const std::string& s) {
+      return std::string{absl::StripSuffix(s, ".0")};
+    };
     // clang-format off
     auto *sc = this->_cli.add_subcommand("evaluate", "Compare Model's output with other contact matrices using various correlation tests.")->fallthrough();
     sc->alias("eval");
@@ -57,12 +60,13 @@ class Cli {
     sc->add_option("-t,--threads", this->_config.nthreads, "CPU threads to allocate.")->check(CLI::PositiveNumber);
     sc->add_option("-o,--output-base-name", this->_config.output_base_name, "Base file name (including directories) to use for output.")->required();
     sc->add_option("--reference-matrix", this->_config.path_to_reference_matrix, "Path to contact matrix to use as reference when computing the correlation. Formats accepted: Cooler.")->required();
+    sc->add_option("-b,--bin-size", this->_config.bin_size, "Bin size in base pairs. Only used when the contact matrix passed to --reference-matrix is in .mcool format.")->check(CLI::PositiveNumber)->transform(remove_trailing_zero_from_floats);
     sc->add_flag("--eucl-dist", this->_config.compute_edist, "Compute Euclidean distance.");
     sc->add_flag("--pearson", this->_config.compute_pearson, "Compute Pearson correlation.");
     sc->add_flag("--spearman", this->_config.compute_spearman, "Compute Spearman rank correlation.");
-    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width to use when computing correlation coefficients.")->check(CLI::NonNegativeNumber)->required();
-    sc->add_option("--sliding-window-size", this->_config.sliding_window_size, "Sliding window size. By default this is set to the diagonal width of ModLE's contact matrix.")->check(CLI::NonNegativeNumber);
-    sc->add_option("--sliding-window-overlap", this->_config.sliding_window_overlap, "Overlap between consecutive sliding-windows.")->check(CLI::NonNegativeNumber)->capture_default_str();
+    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width to use when computing correlation coefficients.")->check(CLI::NonNegativeNumber)->transform(remove_trailing_zero_from_floats)->required();
+    sc->add_option("--sliding-window-size", this->_config.sliding_window_size, "Sliding window size. By default this is set to the diagonal width of ModLE's contact matrix.")->check(CLI::NonNegativeNumber)->transform(remove_trailing_zero_from_floats);
+    sc->add_option("--sliding-window-overlap", this->_config.sliding_window_overlap, "Overlap between consecutive sliding-windows.")->check(CLI::NonNegativeNumber)->transform(remove_trailing_zero_from_floats)->capture_default_str();
     sc->add_option("--depletion-multiplier", this->_config.depletion_multiplier, "Multiplier used to control the magnitude of the depletion.")->check(CLI::NonNegativeNumber)->capture_default_str();
     sc->add_flag("--deplete-reference-contacts,!--no-deplete-reference-contacts", this->_config.deplete_contacts_from_reference, "Deplete contacts along the diagonal from the reference matrix.")->capture_default_str();
     // clang-format on
@@ -87,11 +91,14 @@ class Cli {
   }
 
   inline void make_noisify_subcommand() {
+    auto remove_trailing_zero_from_floats = [](const std::string& s) {
+      return std::string{absl::StripSuffix(s, ".0")};
+    };
     // clang-format off
     auto *sc = this->_cli.add_subcommand("noisify", "Add noise to ModLE's contact matrix in Cooler format.")->fallthrough();
     sc->add_option("-i,--input", this->_config.path_to_input_matrix, "Path to a contact matrix in Cooler format")->check(CLI::ExistingFile)->required();
-    sc->add_option("-o,--output-base-name", this->_config.path_to_output_matrix, "Output file name (possibly including directories) to use for output.")->required();
-    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width of the input contact matrix.")->check(CLI::NonNegativeNumber)->required();
+    sc->add_option("-o,--output-name", this->_config.path_to_output_matrix, "Output file name (possibly including directories) to use for output.")->required();
+    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width of the input contact matrix.")->check(CLI::NonNegativeNumber)->transform(remove_trailing_zero_from_floats)->required();
     sc->add_flag("-f,--force", this->_config.force, "Overwrite existing file(s).")->capture_default_str();
     sc->add_option("--mu,--location", this->_config.genextreme_mu, "Location parameter (mu) of the generalized extreme value used to add noise to the contact matrix.")->check(CLI::NonNegativeNumber)->capture_default_str();
     sc->add_option("--sigma,--scale", this->_config.genextreme_sigma, "Scale parameter (sigma) of the generalized extreme value used to add noise to the contact matrix.")->check(CLI::NonNegativeNumber)->capture_default_str();
@@ -101,6 +108,9 @@ class Cli {
   }
 
   inline void make_stats_subcommand() {
+    auto remove_trailing_zero_from_floats = [](const std::string& s) {
+      return std::string{absl::StripSuffix(s, ".0")};
+    };
     // clang-format off
     auto *sc = this->_cli.add_subcommand("statistics", "Compute several useful statistics for a given Cooler file.")->fallthrough();
     sc->alias("stats");
@@ -108,7 +118,7 @@ class Cli {
     sc->add_option("--chromosome-subrange-file", this->_config.path_to_chrom_subranges, "Path to BED file with subranges of the chromosomes to be processed.")->check(CLI::ExistingFile);
     sc->add_flag("-f,--force", this->_config.force, "Overwrite existing file(s).")->capture_default_str();
     sc->add_option("--bin-size", this->_config.bin_size, "Bin size to use when calculating the statistics. Required in case of MCool files.")->check(CLI::PositiveNumber);
-    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width.")->check(CLI::PositiveNumber)->required();
+    sc->add_option("-w,--diagonal-width", this->_config.diagonal_width, "Diagonal width.")->check(CLI::PositiveNumber)->transform(remove_trailing_zero_from_floats)->required();
     sc->add_flag("--dump-depleted-matrices", this->_config.dump_depleted_matrices, "Dump contact matrices used to calculate the normalized average contact density.");
     sc->add_option("--path-to-histograms", this->_config.output_path_for_histograms, "Path where to output contact histograms.");
     sc->add_option("--exclude-chromosomes", this->_config.chromosomes_excluded_vect, "Comma-separated list of chromosomes to skip when calculating chromosome statistics.")->delimiter(',');
@@ -137,6 +147,12 @@ class Cli {
       absl::StrAppendFormat(
           &errors, "--output-dir should point to a directory or a non-existing path. Is \"%s\"",
           c.output_base_name.string());
+    }
+
+    if (c.path_to_reference_matrix.extension() == ".mcool" && c.bin_size == 0) {
+      absl::StrAppend(&errors,
+                      "--bin-size is required when the contact matrix passed with "
+                      "--reference-matrix is in .mcool format.\n");
     }
 
     if (const auto* s = "/modle_tools/"; !absl::EndsWithIgnoreCase(c.tmp_dir.string(), s)) {
