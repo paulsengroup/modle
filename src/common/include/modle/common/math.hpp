aa982fb39738cf78a52277506df23d642746ec49
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-09-01 12:40:05 +0200

    Bugfix

--- src/common/include/modle/common/math.hpp
+++ src/common/include/modle/common/math.hpp
@@ -14,9 +14,9 @@ constexpr T abs_diff(T a, T b) noexcept;
 //  Mean
 //
 // Compute the arithmetic mean of the items between the begin and end iterators
-template <
-    class InputIt, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, typename UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double mean(InputIt begin, InputIt end, UnaryOperation op = utils::identity());
 /////////
 
@@ -24,56 +24,56 @@ template <
 //
 // Compute the moving average of the items between the begin and end iterators given \p window_size
 // If \p window_size >= std::distance(begin, end), return math::mean(begin, end)
-template <
-    class InputIt, class OutputIt, class I, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_integral_v<I> &&
-                             std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class OutputIt, class I, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_integral_v<I> &&
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 inline size_t moving_average(InputIt input_begin, InputIt input_end, OutputIt output_begin,
                              I window_size, UnaryOperation op = utils::identity());
 /////////
 
 //  Sum of squared deviations
 //
-template <
-    class InputIt, class FP, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_floating_point_v<FP> &&
-                             std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class FP, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_floating_point_v<FP> &&
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double sum_of_squared_deviations(InputIt input_begin, InputIt input_end,
                                                       FP sample_mean,
                                                       UnaryOperation op = utils::identity());
-template <
-    class InputIt, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double sum_of_squared_deviations(InputIt input_begin, InputIt input_end,
                                                       UnaryOperation op = utils::identity());
 /////////
 
 //  Variance
 //
-template <
-    class InputIt, class FP, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_floating_point_v<FP> &&
-                             std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class FP, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_floating_point_v<FP> &&
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double variance(InputIt input_begin, InputIt input_end, FP sample_mean,
                                      UnaryOperation op = utils::identity());
-template <
-    class InputIt, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double variance(InputIt input_begin, InputIt input_end,
                                      UnaryOperation op = utils::identity());
 /////////
 
 //  Standard deviation
 //
-template <
-    class InputIt, class FP, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_floating_point_v<FP> &&
-                             std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class FP, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_floating_point_v<FP> &&
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double standard_dev(InputIt input_begin, InputIt input_end, FP sample_mean,
                                          UnaryOperation op = utils::identity());
-template <
-    class InputIt, class UnaryOperation = utils::identity,
-    class = std::enable_if_t<std::is_invocable_v<UnaryOperation, typename InputIt::value_type>>>
+template <class InputIt, class UnaryOperation = utils::identity,
+          class = std::enable_if_t<
+              std::is_invocable_v<UnaryOperation, decltype(*std::declval<InputIt>())>>>
 [[nodiscard]] inline double standard_dev(InputIt input_begin, InputIt input_end,
                                          UnaryOperation op = utils::identity());
 /////////
