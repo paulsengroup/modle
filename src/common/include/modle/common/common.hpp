2637eaa687465bc5b78320882095b1d9d26851be
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2021-09-12 14:42:20 +0200

    Define MODLE_VERSION_* preproc. variables
    [no ci]

--- src/common/include/modle/common/common.hpp
+++ src/common/include/modle/common/common.hpp
@@ -7,7 +7,11 @@
 #include <cstddef>  // IWYU pragma: keep for size_t
 #include <cstdint>  // for uint32_t, uint_fast8_t
 
-#include "modle/common/utils.hpp"  // for ndebug_defined
+// These variables are defined by CMake based on the value set through the project() command
+#if !defined(MODLE_VERSION_MAJOR) || !defined(MODLE_VERSION_MINOR) || !defined(MODLE_VERSION_PATCH)
+#error \
+    "Project was not configured properly: MODLE_VERSION_* preprocessor variables do not appear to be defined"
+#endif
 
 namespace modle {
 
