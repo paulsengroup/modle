123768b923742cb07fbc7a38a01fb6ceec7929a6
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-02-20 01:39:52 +0100

    Modernize build system
    [no ci]

--- cmake/FetchExternalDeps.cmake
+++ cmake/FetchExternalDeps.cmake
@@ -6,55 +6,66 @@ include(FetchContent)
 
 # cmake-format: off
 FetchContent_Declare(
-  libBigWig
-  URL ${CMAKE_CURRENT_SOURCE_DIR}/external/libBigWig-0.4.6.tar.xz
-  URL_HASH SHA512=11cfb35da7fa99fe8f73d219654d2cfb838a74b7c6ba5610b826265251bdfcfb4feb2c6e2fc2377edd73154dd971f9604ea0e270bd5830d1f28509f84ad49f7e
+        bitflags
+        URL ${CMAKE_CURRENT_SOURCE_DIR}/external/bitflags-1.5.0.tar.xz
+        URL_HASH SHA512=918b73fd40ce6180c237caae221c0d8bea74b203d75a77ee2c399cbf1e063894f1df4a838d13fd87ca0870551ff83449462f8705e61e1c21c7f3c1e47ba07b71
 )
 FetchContent_Declare(
-  mscharconv
-  URL ${CMAKE_CURRENT_SOURCE_DIR}/external/mscharconv.tar.xz
-  URL_HASH SHA512=4378f5be5336c726c3c9d104941a45379484519cb34d61001c408c496b47b53547c9af3a82c9cd0eb332df0c97d244e2f6a20a34aa437cb304980e567e364c2c
+        libBigWig
+        URL ${CMAKE_CURRENT_SOURCE_DIR}/external/libBigWig-0.4.6.tar.xz
+        URL_HASH SHA512=11cfb35da7fa99fe8f73d219654d2cfb838a74b7c6ba5610b826265251bdfcfb4feb2c6e2fc2377edd73154dd971f9604ea0e270bd5830d1f28509f84ad49f7e
 )
 FetchContent_Declare(
-  thread-pool
-  URL ${CMAKE_CURRENT_SOURCE_DIR}/external/thread-pool-2.0.0.tar.xz
-  URL_HASH SHA512=71fa39216842c4759a6eb1a68b37b30a92c017c3753ad21869010aa4a898ea573aeaec85deb641fbb837b9fe1a4641813d97db72f89430abeb24663be8bda4dd
+        mscharconv
+        URL ${CMAKE_CURRENT_SOURCE_DIR}/external/mscharconv.tar.xz
+        URL_HASH SHA512=4378f5be5336c726c3c9d104941a45379484519cb34d61001c408c496b47b53547c9af3a82c9cd0eb332df0c97d244e2f6a20a34aa437cb304980e567e364c2c
 )
 FetchContent_Declare(
-  Xoshiro-cpp
-  URL ${CMAKE_CURRENT_SOURCE_DIR}/external/Xoshiro-cpp-1.1.tar.xz
-  URL_HASH SHA512=fb584cae675ebdb181801237a1462b0931478cb3123987b06dee8cbb4b6d823fcfa148f38aef184dd3192c985f6fe1984339bb2d5e1399db40501ab81a92ecfb
+        thread-pool
+        URL ${CMAKE_CURRENT_SOURCE_DIR}/external/thread-pool-2.0.0.tar.xz
+        URL_HASH SHA512=71fa39216842c4759a6eb1a68b37b30a92c017c3753ad21869010aa4a898ea573aeaec85deb641fbb837b9fe1a4641813d97db72f89430abeb24663be8bda4dd
+)
+FetchContent_Declare(
+        Xoshiro-cpp
+        URL ${CMAKE_CURRENT_SOURCE_DIR}/external/Xoshiro-cpp-1.1.tar.xz
+        URL_HASH SHA512=fb584cae675ebdb181801237a1462b0931478cb3123987b06dee8cbb4b6d823fcfa148f38aef184dd3192c985f6fe1984339bb2d5e1399db40501ab81a92ecfb
 )
 # cmake-format: on
 
+FetchContent_GetProperties(bitflags)
 FetchContent_GetProperties(libBigWig)
 FetchContent_GetProperties(mscharconv)
 FetchContent_GetProperties(thread-pool)
 FetchContent_GetProperties(Xoshiro)
 
+if (NOT ${bitflags}_POPULATED)
+  FetchContent_Populate(bitflags)
+endif ()
+add_subdirectory(${bitflags_SOURCE_DIR} ${bitflags_BINARY_DIR} EXCLUDE_FROM_ALL)
+
 set(WITH_CURL
-    OFF
-    CACHE INTERNAL "")
-if(NOT ${libbigwig}_POPULATED)
+        OFF
+        CACHE INTERNAL "")
+if (NOT ${libbigwig}_POPULATED)
   FetchContent_Populate(libBigWig)
-endif()
+endif ()
 add_subdirectory(${libbigwig_SOURCE_DIR} ${libbigwig_BINARY_DIR} EXCLUDE_FROM_ALL)
 
-if(NOT ${mscharconv}_POPULATED)
+if (NOT ${mscharconv}_POPULATED)
   FetchContent_Populate(mscharconv)
-endif()
+endif ()
 add_subdirectory(${mscharconv_SOURCE_DIR} ${mscharconv_BINARY_DIR})
 
-if(NOT ${thread-pool}_POPULATED)
+if (NOT ${thread-pool}_POPULATED)
   FetchContent_Populate(thread-pool)
-endif()
+endif ()
 add_subdirectory(${thread-pool_SOURCE_DIR} ${thread-pool_BINARY_DIR})
 
-if(NOT MODLE_USE_MERSENNE_TWISTER)
-  if(NOT ${xoshiro-cpp}_POPULATED)
+if (NOT MODLE_USE_MERSENNE_TWISTER)
+  if (NOT ${xoshiro-cpp}_POPULATED)
     FetchContent_Populate(Xoshiro-cpp)
-  endif()
+  endif ()
   add_subdirectory(${xoshiro-cpp_SOURCE_DIR} ${xoshiro-cpp_BINARY_DIR})
-else()
+else ()
   target_compile_definitions(project_options INTERFACE MODLE_USE_MERSENNE_TWISTER=1)
-endif()
+endif ()
