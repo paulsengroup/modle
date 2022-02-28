aa6101fc68c4c6bc3ca1feb405916377353beebf
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-02-28 16:49:27 +0100

    Remove unused scripts

--- scripts/compute_statistics_for_bwigs/precompile_app.jl
+++
@@ -1,5 +0,0 @@
-using compute_statistics_for_bwigs
-
-append!(ARGS, ["--chr-subranges-bed", "/tmp/dummy_data/chr_subranges.bed", "--extrusion-barriers-bed", "/tmp/dummy_data/extr_barriers.bed", "/tmp/dummy_data/sample.txt"])
-compute_statistics_for_bwigs.julia_main()
-
