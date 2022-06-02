86e571859240ec7c75d8976852d8542232574d9d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-06-02 15:51:45 +0200

    Suppress GCC 12 warning issued when including <catch2/catch.hpp>

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -4,7 +4,13 @@
 
 #define CATCH_CONFIG_MAIN
 
+// clang-format off
+#include "modle/common/suppress_compiler_warnings.hpp"
+DISABLE_WARNING_PUSH
+DISABLE_WARNING_MAYBE_UNINITIALIZED
 #include <catch2/catch.hpp>
+DISABLE_WARNING_POP
+// clang-format on
 
 #include "modle/test/self_deleting_folder.hpp"  // for SelfDeletingFolder
 
