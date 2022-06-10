14735b18a594fe29dd32977986c51770dc483c6d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-06-10 18:49:27 +0200

    Try to deal once and for all with spurious CI failures

--- test/units/include/modle/test/self_deleting_folder.hpp
+++ test/units/include/modle/test/self_deleting_folder.hpp
@@ -4,14 +4,17 @@
 
 #pragma once
 
+#include <absl/strings/match.h>  // for EndsWith, StartsWith
+
 #include <atomic>                           // for atomic
+#include <boost/filesystem/exception.hpp>   // for filesystem_error
 #include <boost/filesystem/operations.hpp>  // for create_directories, remove_all, temp_director...
 #include <boost/filesystem/path.hpp>        // for path, operator/
 #include <utility>                          // for move
 
 namespace modle::test {
 // The point of this class is to provide a reliable way to create a directory that automatically
-// deletes istelf as well as its content by default.
+// deletes istelf and its content by default.
 // This can be prevented by setting the internal flag to true.
 // The default constructor will create a unique, randomly named directory under the system tmpdir
 class SelfDeletingFolder {
@@ -19,10 +22,21 @@ class SelfDeletingFolder {
   std::atomic<bool> _delete_on_destruction{true};
 
  public:
-  [[maybe_unused]] SelfDeletingFolder()
-      : _path(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path()) {
+  [[maybe_unused]] SelfDeletingFolder() {
+    boost::filesystem::path tmpdir{};
+    try {
+      tmpdir = boost::filesystem::temp_directory_path();
+    } catch (const boost::filesystem::filesystem_error& e) {
+      // Workaround spurious CI failures due to missing /tmp folder exception
+      assert(absl::StartsWith(e.what(), "boost::filesystem::temp_directory_path: Not a directory"));
+      assert(absl::EndsWith(e.what(), "\"/tmp\""));
+      tmpdir = "test/data/unit_tests/scratch";
+    }
+
+    _path = tmpdir / boost::filesystem::unique_path();
     boost::filesystem::create_directories(_path);
   }
+
   [[maybe_unused]] explicit SelfDeletingFolder(boost::filesystem::path path,
                                                bool delete_on_destruction = true)
       : _path(std::move(path)), _delete_on_destruction(delete_on_destruction) {
