da03a28e1047afb796755266a7b10998f785c810
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-10-26 00:35:28 +0200

    Update Dockerfile and build_dockerfile.sh
    - Update Dockerfile to work with the new integration tests using cooler
      instead of h5diff
    - Update Dockerfile to improve layer caching
    - Update build_dockerfile.sh with the new variables required by the
      Dockerfile

--- utils/devel/build_dockerfile.sh
+++ utils/devel/build_dockerfile.sh
@@ -8,30 +8,55 @@ set -e
 set -u
 set -o pipefail
 
-img_name='modle'
-ver="$(git rev-parse --short HEAD)"
-if ! git diff-index --quiet HEAD --; then
-  ver+="-dirty"
+IMAGE_NAME='modle'
+
+GIT_HASH="$(git rev-parse HEAD)"
+GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
+GIT_TAG="$(git for-each-ref 'refs/tags/v*.*.*' --count 1 --sort=-v:refname --format "%(refname:short)"  --points-at HEAD)"
+CREATION_DATE="$(date --iso-8601)"
+
+if [[ $(git status --porcelain -uno) ]]; then
+  GIT_IS_DIRTY=1
+else
+  GIT_IS_DIRTY=0
 fi
 
-echo "Building \"$img_name:$ver\""
+FINAL_BASE_IMAGE_DIGEST="$(sudo docker inspect --format='{{index .RepoDigests 0}}' docker.io/library/ubuntu:22.04 | grep -o '[[:alnum:]:]\+$')"
+
+IMAGE_TAG="$GIT_SHORT_HASH"
+if [ $GIT_IS_DIRTY -ne 0 ]; then
+  IMAGE_TAG+='-dirty'
+fi
+
+if [ -z "$GIT_TAG" ]; then
+  GIT_TAG='unknown'
+fi
+
+2>&1 echo "Building \"$IMAGE_NAME:$IMAGE_TAG\"..."
 
 sudo docker pull docker.io/library/ubuntu:22.04
+BUILD_BASE_IMAGE='ghcr.io/paulsengroup/ci-docker-images/ubuntu-22.04-cxx-clang-14:latest'
+TEST_BASE_IMAGE='ghcr.io/paulsengroup/ci-docker-images/modle/ubuntu-22.04-cxx-clang-14:latest'
+
+sudo docker pull "$BUILD_BASE_IMAGE"
+sudo docker pull "$TEST_BASE_IMAGE"
 
 sudo docker build \
-  --build-arg "BUILD_BASE_IMAGE=ghcr.io/paulsengroup/ci-docker-images/modle/ubuntu-22.04-cxx-clang-14:latest" \
-  --build-arg "TEST_BASE_IMAGE=ghcr.io/paulsengroup/ci-docker-images/modle/ubuntu-22.04-cxx-clang-14:latest" \
+  --build-arg "BUILD_BASE_IMAGE=$BUILD_BASE_IMAGE" \
+  --build-arg "TEST_BASE_IMAGE=$TEST_BASE_IMAGE" \
   --build-arg "FINAL_BASE_IMAGE=docker.io/library/ubuntu" \
   --build-arg "FINAL_BASE_IMAGE_TAG=22.04" \
-  --build-arg "FINAL_BASE_IMAGE_DIGEST=$(sudo docker inspect --format='{{index .RepoDigests 0}}' docker.io/library/ubuntu:22.04 | grep -o '[[:alnum:]:]\+$')" \
+  --build-arg "FINAL_BASE_IMAGE_DIGEST=$FINAL_BASE_IMAGE_DIGEST" \
   --build-arg "C_COMPILER=clang-14" \
   --build-arg "CXX_COMPILER=clang++-14" \
-  --build-arg "GIT_HASH=$(git rev-parse HEAD)" \
-  --build-arg "GIT_SHORT_HASH=$(git rev-parse --short HEAD)" \
-  --build-arg "CREATION_DATE=$(date --iso-8601)" \
-  -t "$img_name:latest" \
-  -t "$img_name:$(date --iso-8601 | tr -d '\-' )" \
-  -t "$img_name:$ver" \
+  --build-arg "GIT_HASH=$GIT_HASH" \
+  --build-arg "GIT_SHORT_HASH=$GIT_SHORT_HASH" \
+  --build-arg "GIT_TAG=$GIT_TAG" \
+  --build-arg "GIT_IS_DIRTY=$GIT_IS_DIRTY" \
+  --build-arg "CREATION_DATE=$CREATION_DATE" \
+  -t "$IMAGE_NAME:latest" \
+  -t "$IMAGE_NAME:$(echo "$CREATION_DATE" | tr -d '\-' )" \
+  -t "$IMAGE_NAME:$IMAGE_TAG" \
   "$(git rev-parse --show-toplevel)"
 
  # sudo singularity build -F "${img_name}_v${ver}.sif" \
