0507af908667ea56bf660195d9f975e3b74599cc
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-12-18 21:16:44 +0100

    Bump Coolerpp

--- cmake/FetchExternalDeps.cmake
+++ cmake/FetchExternalDeps.cmake
@@ -8,10 +8,10 @@ include(FetchContent)
 FetchContent_Declare(
         coolerpp
         GIT_REPOSITORY https://github.com/robomics/coolerpp.git
-        GIT_TAG d4e5d6222a21feedea7c101469238b451144047c
+        GIT_TAG 54ee8f5063eab4a4f1a54f113cb01d6b2c01a499
 )
 # cmake-format: on
 
-SET(ENABLE_TESTING OFF)
-SET(BUILD_EXAMPLES OFF)
+SET(COOLERPP_ENABLE_TESTING OFF)
+SET(COOLERPP_BUILD_EXAMPLES OFF)
 FetchContent_MakeAvailable(coolerpp)
