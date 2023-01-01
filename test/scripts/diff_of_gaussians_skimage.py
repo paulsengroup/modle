493d6978ba1db56da0fefefd87141305f75a1d32
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-01 19:29:12 +0100

    Format Python scripts with Black

--- test/scripts/diff_of_gaussians_skimage.py
+++ test/scripts/diff_of_gaussians_skimage.py
@@ -12,30 +12,30 @@ from skimage.filters import difference_of_gaussians
 
 
 def make_cli() -> argparse.ArgumentParser:
-  cli = argparse.ArgumentParser()
+    cli = argparse.ArgumentParser()
 
-  cli.add_argument("sigma1", type=float)
-  cli.add_argument("sigma2", type=float)
+    cli.add_argument("sigma1", type=float)
+    cli.add_argument("sigma2", type=float)
 
-  cli.add_argument("--cutoff", type=float, default=3.0)
+    cli.add_argument("--cutoff", type=float, default=3.0)
 
-  cli.add_argument("--sep", type=lambda s: "\t" if s == "\\t" else s, default="\t")
+    cli.add_argument("--sep", type=lambda s: "\t" if s == "\\t" else s, default="\t")
 
-  cli.add_argument("--extension-mode", type=str, default="nearest")
+    cli.add_argument("--extension-mode", type=str, default="nearest")
 
-  return cli
+    return cli
 
 
 def main():
-  args = vars(make_cli().parse_args())
+    args = vars(make_cli().parse_args())
 
-  sep = args["sep"]
+    sep = args["sep"]
 
-  m = np.loadtxt(sys.stdin, dtype=float, delimiter=sep)
-  m = difference_of_gaussians(m, args["sigma1"], args["sigma2"], mode=args["extension_mode"], truncate=args["cutoff"])
+    m = np.loadtxt(sys.stdin, dtype=float, delimiter=sep)
+    m = difference_of_gaussians(m, args["sigma1"], args["sigma2"], mode=args["extension_mode"], truncate=args["cutoff"])
 
-  np.savetxt(sys.stdout, m, fmt="%.18g", delimiter=sep)
+    np.savetxt(sys.stdout, m, fmt="%.18g", delimiter=sep)
 
 
 if __name__ == "__main__":
-  main()
+    main()
