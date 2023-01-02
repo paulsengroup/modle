0c6555bdd5e250a2cc6350c0f035118769b52f26
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-02 12:53:29 +0100

    Update generate_reference_2d_gauss_kernels.sh

--- test/scripts/generate_reference_2d_gauss_kernels.sh
+++ test/scripts/generate_reference_2d_gauss_kernels.sh
@@ -9,26 +9,18 @@ set -e
 set -o pipefail
 set -u
 
-tmpdir="$(mktemp -d)"
-trap 'rm -rf "$tmpdir"' EXIT
-
-wd="$tmpdir/reference_gaussian_kernels"
-mkdir "$wd"
-
 git_root="$(git rev-parse --show-toplevel)"
 PATH="$git_root/test/scripts:$PATH"
-dest="$git_root/test/data/unit_tests/reference_gaussian_kernels.tar.xz"
+dest_dir="$git_root/test/data/unit_tests/reference_gaussian_kernels/"
+
+mkdir -p "$dest_dir"
 
 for size in $(seq 1 15); do
   for sigma in 0.5 1.0 1.5 2.5 6.3 10.0; do
     generate_2d_gauss_kernel.py "$size" "$sigma" |
-      tee "$wd/gaussian_kernel_${size}_${sigma}.csv" > /dev/null
+      xz -9 --extreme |
+      tee "$dest_dir/gaussian_kernel_${size}_${sigma}.tsv.xz" > /dev/null &
   done
 done
 
-mkdir -p "$(dirname "$dest")"
-
-tar -C "$tmpdir" -cf - "$(basename "$wd")" |
-  xz -9 --extreme | tee "$dest" > /dev/null
-
-2>&1 echo "$dest"
+wait
