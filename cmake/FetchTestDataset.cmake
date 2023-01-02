bd4bdaedb6f3c7caa042d8ed91ccf936329414c8
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-02 22:50:09 +0100

    Update test dataset

--- cmake/FetchTestDataset.cmake
+++ cmake/FetchTestDataset.cmake
@@ -4,8 +4,8 @@
 
 # cmake-format: off
 file(
-  DOWNLOAD https://www.dropbox.com/s/avqohi7xzj0voi2/modle_test_data.tar.xz?dl=1
-  EXPECTED_HASH 65f287835560917382097acb22ace9f086338d988cce0827188acc846e18b7de8b8c380a4145e51d81ebd2bf5d0282af12dd7b0dd955f32bebcdfd2348d5f148
+  DOWNLOAD https://zenodo.org/record/7499993/files/modle_test_data.tar.xz?download=1
+  EXPECTED_HASH SHA512=90d7899f9f86d86c87bd79de1a88b237e8de6265510ecdb8ca046204c26fd7752d76066b1bb17b55c3b03c876258a2ac25fa533a12f280ddbafbbb2c8a15c2bf
   ${CMAKE_CURRENT_SOURCE_DIR}/test/data/modle_test_data.tar.xz)
 # cmake-format: on
 
