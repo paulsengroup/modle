1dd284761977727bd653705c867cd2ef3ad44316
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-05 20:11:23 +0100

    Update integration test scripts (#74)
    - Drop requirement for shasum, use cmp instead
    - Refactor code to detect missing input files
    - Improve status updates when files are identical
    
    Closes #73

--- test/scripts/modle_tools_transform_integration_test.sh
+++ test/scripts/modle_tools_transform_integration_test.sh
@@ -8,6 +8,46 @@ set -e
 set -o pipefail
 set -u
 
+# readlink -f is not available on macos...
+function readlink_py {
+  set -eu
+  python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$1"
+}
+
+function check_files_exist {
+  set -eu
+  status=0
+  for f in "$@"; do
+    if [ ! -f "$f" ]; then
+      2>&1 echo "Unable to find test file \"$f\""
+      status=1
+    fi
+  done
+
+  return "$status"
+}
+
+function compare_coolers {
+  set -o pipefail
+  set -e
+
+  2>&1 echo "Comparing $1 with $2..."
+  if diff <(cooler dump -t chroms "$1") \
+          <(cooler dump -t chroms "$2") \
+     && \
+     diff <(cooler dump --join "$1") \
+          <(cooler dump --join "$2");
+  then
+    2>&1 echo "Files are identical"
+    return 0
+  else
+    2>&1 echo "Files differ"
+    return 1
+  fi
+}
+
+export function readlink_py
+
 status=0
 
 if [ $# -ne 1 ]; then
@@ -30,23 +70,13 @@ fi
 
 modle_tools_bin="$1"
 
-data_dir="$(readlink -f "$(dirname "$0")/../data/integration_tests")"
+data_dir="$(readlink_py "$(dirname "$0")/../data/integration_tests")"
 
 matrix="$data_dir/4DNFI9GMP2J8_chr20_25kbp.cool"
 matrix_blurred="$data_dir/4DNFI9GMP2J8_chr20_25kbp_blurred.cool"
 matrix_dog="$data_dir/4DNFI9GMP2J8_chr20_25kbp_dog.cool"
 
-if [ ! -f "$matrix_blurred" ]; then
-  2>&1 echo "Unable to find test file \"$matrix_blurred\""
-  status=1
-fi
-
-if [ ! -f "$matrix_dog" ]; then
-  2>&1 echo "Unable to find test file \"$matrix_dog\""
-  status=1
-fi
-
-if [ ! "$status" -eq 0 ]; then
+if ! check_files_exist "$matrix" "$matrix_blurred" "$matrix_dog"; then
   exit 1
 fi
 
@@ -66,27 +96,6 @@ trap 'rm -rf -- "$outdir"' EXIT
 # mkdir -p /tmp/test/data/integration_tests/
 # cp "$outdir/out_blurred.cool" /tmp/test/data/integration_tests/4DNFI9GMP2J8_chr20_25kbp_blurred.cool
 # cp "$outdir/out_dog.cool" /tmp/test/data/integration_tests/4DNFI9GMP2J8_chr20_25kbp_dog.cool
-
-function compare_coolers {
-  set -o pipefail
-  set -e
-
-  2>&1 echo "Comparing $1 with $2..."
-  if diff <(cooler dump -t chroms "$1") \
-          <(cooler dump -t chroms "$2") \
-     && \
-     diff <(cooler dump --join "$1") \
-          <(cooler dump --join "$2");
-  then
-    2>&1 echo "Files are identical"
-    return 0
-  else
-    2>&1 echo "Files differ"
-    return 1
-  fi
-}
-
-status=0
 
 if ! compare_coolers "$outdir/out_blurred.cool" "$data_dir/4DNFI9GMP2J8_chr20_25kbp_blurred.cool"; then
   status=1
