d5f78ecae108e39a6483c35e84d37c3f7f8e61ae
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-05 18:13:55 +0100

    Update test/scripts/devel/archive_test_data.sh

--- test/scripts/devel/archive_test_data.sh
+++ test/scripts/devel/archive_test_data.sh
@@ -13,16 +13,18 @@ set -u
 git_root="$(git rev-parse --show-toplevel)"
 
 (cd "$git_root/test/data" &&
-  find . -type f -exec sha256sum {} + |
-      grep -vF checksums.sha256 |
-      grep -vF README.md |
-      grep -vF modle_test_data.tar.xz |
+  find . -type f \
+         \( -path './integration_tests/*' \
+            -or \
+            -path './unit_tests/*' \) \
+         -exec sha256sum {} + |
       sort -k2,2V |
       tee checksums.sha256 > /dev/null
 )
 
-tar -C "$git_root" -cf - test/data/{integration,unit}_tests/ |
+tar -C "$git_root" -cf - test/data/{integration,unit}_tests/ test/data/checksums.sha256 |
   xz -9 --extreme -T0 |
   tee "$git_root/test/data/modle_test_data.tar.xz" > /dev/null
 
+tar -tf "$git_root/test/data/modle_test_data.tar.xz"
 sha512sum "$git_root/test/data/modle_test_data.tar.xz"
