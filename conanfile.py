5c7c2f539c681649f224d684646068150adfc67b
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-13 11:32:25 +0100

    Bump deps

--- conanfile.py
+++ conanfile.py
@@ -20,18 +20,18 @@ class MoDLE(ConanFile):
     requires = [
         "abseil/20220623.1@#266f2e66c0dcd428efb9b24c2a5c8f05",
         "bitflags/1.5.0@#f49a584785e75c40bf6970615c452b4e",
-        "boost/1.81.0@#ceb5cbef5143b43eab0a8e824238eba3",
+        "boost/1.81.0@#2a6097db2254b5ea31fcb54377982c4b",
         "bshoshany-thread-pool/3.3.0@#22e99aee6babc19e679754d95dad2de4",
         "bzip2/1.0.8@#464be69744fa6d48ed01928cfe470008",
         "catch2/3.2.1@#f4c25988de20c21a24a1a24dab87e9b5",
-        "cli11/2.3.1@#8b591d97a2ed21d1e8d50afb67f3f97b",
+        "cli11/2.3.2@#8ccdf14fb1ad53532d498c16ae580b4b",
         "concurrentqueue/1.0.3@#c1cb7d960d8b64073643b45fa63f0bd1",
         "cpp-sort/1.14.0@#3453aaaf83c1dae4214ca3b5c4c3a5c8",
         "fast_float/3.8.1@#ef5291bee6d3ad59f986096df16f43e6",
         "fmt/9.1.0@#811e918ca4b4e0b9ddd6d5a2883efa82",
         "hdf5/1.12.2@#dc802e78ddfbfd6dac7a31cc004c8db0",  # Coolerpp
         "highfive/2.6.2@#829275c7502abf62045167adb79ee581",  # Coolerpp
-        "libarchive/3.6.2@#a91b4b509a41442070757bcf295b2794",
+        "libarchive/3.6.2@#43ce22888cd77d1306e332b41fce8d6c",
         "libbigwig/0.4.7@#c51cf59700963a74fe0089a81293c91c",
         "libcuckoo/0.3.1@#2fadf7043e85444c19c1ededf9e43653",
         "lz4/1.9.4@#8699136f62681eeba3dc033f85cc63e2",
@@ -43,8 +43,8 @@ class MoDLE(ConanFile):
         "tsl-hopscotch-map/2.3.0@#497d3f41172cefe2df9ac17692c52734",  # Coolerpp
         "tsl-ordered-map/1.0.0@#dceec5bd0243a5c659939d0295dfee50",  # Coolerpp
         "xoshiro-cpp/1.1@#be8a2825995d67cf2fb26f01accb4f4d",
-        "xxhash/0.8.1@#5b6ded9ec554b9abb8aae075d2fd5846",
-        "xz_utils/5.2.5@#7315e0f635fed3f9a91b8bfd5456b72c",
+        "xxhash/0.8.1@#b60fcc5f9821c988a155935d87562e1d",
+        "xz_utils/5.4.0@#394283edeac4658356894312aa0fdcbb",
         "zlib/1.2.13@#13c96f538b52e1600c40b88994de240f",
         "zstd/1.5.2@#485dd9c1f9245f0f362730e8b8031a17",
     ]
